d8316196242ae5fd65e809638863660d14d252a2
unknown <andreygor0502@yandex.ru>
2021-01-06 17:00:44 +1100

    done app

---
+++ clientNetwork/client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace clientNetwork
+{
+    class client
+    {
+        const string IpServer = "127.0.0.1";
+        const int PORT = 8080;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient clientSocket = new TcpClient();
+                clientSocket.Connect(IpServer, PORT);
+
+                NetworkStream netStream = clientSocket.GetStream();
+
+                StreamWriter writer = new StreamWriter(netStream);
+
+                Console.Write("Write any string: ");
+                writer.WriteLine(Console.ReadLine());
+                writer.Flush();
+
+                StreamReader reader = new StreamReader(netStream);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Response from server: {0}", reader.ReadLine());
+
+                writer.Close();
+                reader.Close();
+                netStream.Close();
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR!\n" + ex.Message);
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
---
+++ serverNetwork/server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace serverNetwork
+{
+    class server
+    {
+        const int PORT = 8080;
+        const string IpServer = "127.0.0.1";
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener serverSocket = new TcpListener(IPAddress.Parse(IpServer), PORT);
+
+                serverSocket.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Wait connection...");
+
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Connection occured!");
+                    Console.ResetColor();
+
+                    NetworkStream payLoadFromClient = clientSocket.GetStream();
+                    StreamReader reader = new StreamReader(payLoadFromClient);
+                    string message = reader.ReadLine();
+
+                    StreamWriter writer = new StreamWriter(payLoadFromClient);
+
+                    writer.WriteLine(message.ToUpper());
+
+                    
+                    writer.Close();
+                    reader.Close();
+                    payLoadFromClient.Close();
+                    clientSocket.Close();
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("ERROR!\n" + ex.Message);
+
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
